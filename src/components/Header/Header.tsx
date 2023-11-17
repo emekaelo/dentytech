c59fac02881edc72b10b496bd774b0179ec3d0f0
emekaelo <elochux@gmail.com>
2023-11-17 19:48:37 +0100

    style: add animation for section base and header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,28 @@ import {Button} from "../Buttons/Button.tsx";
 import {IconWrapper} from "../IconWrapper/IconWrapper.tsx";
 import {Tooth} from "@phosphor-icons/react";
 import Menu from "../../assets/icons/Menu.tsx";
+import {useLayoutEffect, useRef} from "react";
+import gsap from "gsap";
 
 export const Header = () => {
+    const headerRef = useRef(null);
+    const tl = useRef<GSAPTimeline>()
+
+    useLayoutEffect(() => {
+        const ctx = gsap.context(() => {
+            tl.current = gsap.timeline().from([".nav", ".app-access__btn"], {
+                y: -50,
+                opacity: 0,
+                scale: 0.5
+            }).from(".brand", {
+                scale: 0
+            }, "<")
+        }, headerRef);
+        return () => ctx.revert();
+    }, [])
+
     return (
-        <header className='header'>
+        <header ref={headerRef} className='header'>
             <nav className="nav">
                 <Button text='Menu' className='ghost-btn' />
                 <div className="nav__icon">
--- src/components/SectionBase/SectionBase.tsx
+++ src/components/SectionBase/SectionBase.tsx
@@ -1,21 +1,61 @@
 import "./SectionBase.scss";
 import { IconWrapper } from "../IconWrapper/IconWrapper.tsx";
-import {FacebookLogo, InstagramLogo, YoutubeLogo} from "@phosphor-icons/react";
+import {
+  FacebookLogo,
+  InstagramLogo,
+  YoutubeLogo,
+} from "@phosphor-icons/react";
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export const SectionBase = () => {
+  const sectionBaseRef = useRef(null);
+  const tl = useRef<GSAPTimeline>();
+  gsap.registerPlugin(ScrollTrigger);
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      tl.current = gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: ".section-base__motto",
+            start: "top bottom",
+            end: "bottom top",
+          },
+        })
+        .from([".section-base__motto", ".section-base__social-award"], {
+          y: 50,
+          opacity: 0,
+          scale: 0.3,
+          duration: 1,
+        })
+        .from(
+          ".section-base__social-icons div",
+          {
+            scale: 0,
+            rotate: "-90deg",
+            duration: 1,
+          },
+          "<"
+        );
+    }, sectionBaseRef);
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <div className="section-base">
+    <div ref={sectionBaseRef} className="section-base">
       <div className="section-base__motto">Your Smile, Our Passion</div>
       <div className="section-base__social">
         <div className="section-base__social-icons">
           <IconWrapper bgColor="#fffcee" width={24}>
-            <InstagramLogo size={16}  weight="thin" />
+            <InstagramLogo size={16} weight="thin" />
           </IconWrapper>
           <IconWrapper bgColor="#fffcee" width={24}>
-            <YoutubeLogo  size={16}  weight="thin" />
+            <YoutubeLogo size={16} weight="thin" />
           </IconWrapper>
           <IconWrapper bgColor="#fffcee" width={24}>
-            <FacebookLogo  size={16}  weight="thin" />
+            <FacebookLogo size={16} weight="thin" />
           </IconWrapper>
         </div>
         <div className="section-base__social-award">Best Start Up of 2023</div>
